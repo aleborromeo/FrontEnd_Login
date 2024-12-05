332804b77f9507dd9a0579340cf80672c0a870be
aleborromeo <172089745+aleborromeo@users.noreply.github.com>
2024-12-04 20:30:04 -0500

    Controlador para ver a los usuarios

--- controllers/controladorUsuario.php
+++ controllers/controladorUsuario.php
@@ -1,9 +1,11 @@
 <?php
-    session_start();
-
     require_once $_SERVER['DOCUMENT_ROOT'].'/etc/config.php';
     require_once $_SERVER['DOCUMENT_ROOT'].'/models/modeloUsuario.php';
     require_once $_SERVER['DOCUMENT_ROOT'].'/views/vistaUsuario.php';
+
+    if (session_status()== PHP_SESSION_NONE){
+        session_start();
+    }
 
     if (!isset($_SESSION["txtusername"])) {
         header('Location: ' . get_UrlBase('index.php'));
