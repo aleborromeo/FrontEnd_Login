730615d3d1dd21fb6fe815ed0ad777fc0e76e69b
aleborromeo <172089745+aleborromeo@users.noreply.github.com>
2024-12-04 20:31:42 -0500

    Controlador para eliminar usuarios

--- controllers/controladorEliminarUsuario.php
+++ controllers/controladorEliminarUsuario.php
@@ -1,9 +1,11 @@
 <?php
-    session_start();
-
     require_once $_SERVER['DOCUMENT_ROOT'].'/etc/config.php';
     require_once $_SERVER['DOCUMENT_ROOT'].'/models/modeloUsuario.php';
     require_once $_SERVER['DOCUMENT_ROOT'].'/views/vistaEliminarUsuario.php';
+
+    if (session_status()== PHP_SESSION_NONE){
+        session_start();
+    }
 
     function verificarAutenticacion()
     {
